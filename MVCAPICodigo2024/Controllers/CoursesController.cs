c9c0c15eb992c6c1a6fb28c52819076e19159237
Hugo Torrico <torricomarquezh@gmail.com>
2025-02-07 21:41:48 -0500

    Agregar archivos de proyecto.

---
+++ MVCAPICodigo2024/Controllers/CoursesController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCAPICodigo2024.Models.Response;
+using System.Text.Json;
+
+namespace MVCAPICodigo2024.Controllers
+{
+    public class CoursesController : Controller
+    {
+
+        string url = "https://localhost:7238/api/Courses";
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> GetAsync(string filter)
+        {
+            HttpClient httpClient = new HttpClient();
+            
+            url = url + "?filter=" + filter;
+
+            HttpResponseMessage response = await httpClient.GetAsync(url);
+
+            List<CourseResponseV1> model = null;
+
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonResponse = await response.Content.ReadAsStringAsync();
+                model = JsonSerializer.Deserialize<List<CourseResponseV1>>(jsonResponse, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+            }
+            else
+            {
+                Console.WriteLine($"Error: {response.StatusCode}");
+                model = new List<CourseResponseV1>();
+            }
+
+            return Json(model);
+        }
+    }
+}
---
+++ MVCAPICodigo2024/Models/Response/CourseResponseV1.cs
@@ -0,0 +1,9 @@
+﻿namespace MVCAPICodigo2024.Models.Response
+{
+    public class CourseResponseV1
+    {
+        public int CourseID { get; set; }
+        public string Name { get; set; }
+        public int Credit { get; set; }        
+    }
+}
